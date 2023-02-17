c547eabd2e5fe6a5bb4cb9c253e6fb863e0ca8af
James Swank <jamesswank@Jamess-Laptop.local>
2023-02-17 11:12:02 -0700

    starts app with header

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from dash import Dash, html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
 
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+
+header = html.Div("Arapahoe Census Tract Data", className="h2 p-2 text-white bg-primary text-center")
+
+app.layout = dbc.Container(
+    [
+        header,
+    ],
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8080)
